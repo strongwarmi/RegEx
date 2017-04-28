6181bc27aa068f69079b6c4c07c99fee63580da5
strongwarmi <elianasc10@hotmail.com>
2017-04-28 12:03:25 -0500

    dni validado

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,24 @@ formulario.addEventListener("submit", function(event) {
 });
 function validateForm(){
 	/* Escribe tú código aquí */
-  name = document.getElementById("name").value;
-  lastname = document.getElementById("lastname").value;
-  date = document.getElementById("date").value;
-  password = document.getElementById("password").value;
-//  nombre = document.getElementById("nombre");
-  var PrimeraLetraMayuscula =  "/^[A-Z].*/";
+  var name = document.getElementById("name").value;
+  var lastname = document.getElementById("lastname").value;
+  var dni = document.getElementById("dni").value;
+  var date = document.getElementById("date").value;
+  var password = document.getElementById("password").value;
+  var nombre = document.getElementById("nombre");
+  var apellido=document.getElementById("apellido");
+  var NumDni = document.getElementById("NumDni");
+  var PrimeraLetraMayuscula =  /^[A-Z].*/ ;
+  var RegExDNI = /[0-9]{8}/ ;
 
   if( name == null || name.length == 0 ||(!PrimeraLetraMayuscula.test(name)) ) {
   nombre.innerHTML = "El nombre debe empezar con mayuscula";
   }
   if( lastname == null || lastname.length == 0 ||(!PrimeraLetraMayuscula.test(lastname)) ) {
-  apellido.innerHTML = "El nombre debe empezar con mayuscula";
+  apellido.innerHTML = "El apellido debe empezar con mayuscula";
+  }
+  if( dni == null || dni.length == 0 ||(!RegExDNI.test(dni)) ) {
+  NumDni.innerHTML = "Dni debe tener 8 digitos y solo numeros";
   }
 }
