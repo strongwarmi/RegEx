f35fee543515b4044f6a376957fd1febc08b64bf
strongwarmi <elianasc10@hotmail.com>
2017-04-28 13:28:52 -0500

    validacion numeros telefonicos

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,38 @@ function validateForm(){
   var lastname = document.getElementById("lastname").value;
   var dni = document.getElementById("dni").value;
   var date = document.getElementById("date").value;
+  var celular = document.getElementById("celular").value;
+  var casa = document.getElementById("casa").value;
   var password = document.getElementById("password").value;
-  var nombre = document.getElementById("nombre");
-  var apellido=document.getElementById("apellido");
+  var Nombre = document.getElementById("Nombre");
+  var Apellido=document.getElementById("Apellido");
+  var FechNac=document.getElementById("FechNac");
   var NumDni = document.getElementById("NumDni");
+  var Celu = document.getElementById("Celu");
+  var Casita =  document.getElementById("Casita");
+
   var PrimeraLetraMayuscula =  /^[A-Z].*/ ;
   var RegExDNI = /[0-9]{8}/ ;
+  var RegFechNac= /[1,2]/ ; //2017-04-28
+  var RegExCelu = /[9][0-9]{8}/ ;
+  var RegExCasa = /[2-7][0-9]{6}/ ;
 
   if( name == null || name.length == 0 ||(!PrimeraLetraMayuscula.test(name)) ) {
-  nombre.innerHTML = "El nombre debe empezar con mayuscula";
+  Nombre.innerHTML = "El nombre debe empezar con mayúscula";
   }
   if( lastname == null || lastname.length == 0 ||(!PrimeraLetraMayuscula.test(lastname)) ) {
-  apellido.innerHTML = "El apellido debe empezar con mayuscula";
+  Apellido.innerHTML = "El apellido debe empezar con mayúscula";
   }
-  if( dni == null || dni.length == 0 ||(!RegExDNI.test(dni)) ) {
-  NumDni.innerHTML = "Dni debe tener 8 digitos y solo numeros";
+  if( dni == null || dni.length == 0 || dni.length>8 ||(!RegExDNI.test(dni)) ) {
+  NumDni.innerHTML = "Dni debe tener 8 digitos";
+  }
+  if( date == null || date.length == 0  ) {
+  FechNac.innerHTML = date;
+  }
+  if( celular == null || celular.length == 0 || celular.length>9 ||(!RegExCelu.test(celular)) ) {
+  Celu.innerHTML = "Numero de celular no válido debe contener 9 caracteres";
+  }
+  if( casa == null || casa.length == 0  || casa.length>7 ||(!RegExCasa.test(casa)) ) {
+  Casita.innerHTML = "Numero de casa no válido debe contener 7 caracteres";
   }
 }
