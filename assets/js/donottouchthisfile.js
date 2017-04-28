7e4f6ca910149919faf67478cdac7a899d9c20f3
strongwarmi <elianasc10@hotmail.com>
2017-04-28 11:23:00 -0500

    validacion primera letra mayuscula

--- assets/js/donottouchthisfile.js
+++
@@ -1,5 +0,0 @@
-var element = document.querySelector(".form-signup");
-element.addEventListener("submit", function(event) {
-  event.preventDefault();
-  validateForm();
-});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,21 @@
+var formulario= document.querySelector(".form-signup");
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+  validateForm();
+});
 function validateForm(){
 	/* Escribe tú código aquí */
   name = document.getElementById("name").value;
   lastname = document.getElementById("lastname").value;
   date = document.getElementById("date").value;
   password = document.getElementById("password").value;
+//  nombre = document.getElementById("nombre");
+  var PrimeraLetraMayuscula =  "/^[A-Z].*/";
 
-  if( name == null || name.length == 0 || /^\s+$/.test(name) ) {
-  nombre.innerHTML = "Debe ingresar su nombre";
+  if( name == null || name.length == 0 ||(!PrimeraLetraMayuscula.test(name)) ) {
+  nombre.innerHTML = "El nombre debe empezar con mayuscula";
   }
-  if( lastname == null || lastname.length == 0 || /^\s+$/.test(lastname) ) {
-  apellido.innerHTML = "Apellido requerido";
-  }
-  if( email == null || email.length == 0 || /^\s+$/.test(email) || !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))) {
-  correo.innerHTML = "Verifique su E-mail";
-  }
-  if( password == null || password.length < 6 || /^\s+$/.test(password) ) {
-  pass.innerHTML = "La contraseña debe tener al menos 6 caracteres";
-  }
-  if( indice == null || indice == 0 ) {
-    var tooltip = document.createElement('span');
-    var msj = document.createTextNode("Debes seleccionar al menos un tipo de bici");
-    tooltip.appendChild(msj);
-    document.getElementsByClassName('input-box')[4].appendChild(tooltip);
-    //console.log(document.getElementsByClassName('input-box'));
+  if( lastname == null || lastname.length == 0 ||(!PrimeraLetraMayuscula.test(lastname)) ) {
+  apellido.innerHTML = "El nombre debe empezar con mayuscula";
   }
 }
